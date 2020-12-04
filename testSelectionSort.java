68e8e7eeef8c5e84d781283c58f2f720799c4564
Daijeline <daijekj@gmail.com>
2020-12-04 03:41:02 -0500

    mixdup add

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -38,12 +38,43 @@ public class testSelectionSort {
 		}
 	public void testMixed(){
         /** Test data contains with both positive, negative and zeros **/
-
+        int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = -14;
+		arr[2] = 7;
+		arr[3] = -5;
+		arr[4] = 2;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -14;
+		Sortedarr[1] = -5;
+		Sortedarr[2] = 2;
+		Sortedarr[3] = 7;
+		Sortedarr[4] = 8;
+		/** add tests to check for this unit test **/
+		SelectionSort test = new SelectionSort();
+		arr = test.basicSelectionSort(arr);
+		assertArrayEquals("MT Failed", Sortedarr, arr);
 
 		}
 	public void testDuplicates(){
 		/** Test data contains duplicates **/
-		
+    int[] arr = new int[5];
+		arr[0] = 1;
+		arr[1] = 1;
+		arr[2] = -2;
+		arr[3] = -2;
+		arr[4] = 3;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -2;
+		Sortedarr[1] = -2;
+		Sortedarr[2] = 1;
+		Sortedarr[3] = 1;
+		Sortedarr[4] = 3;
+		/** add tests to check for this unit test **/
+		SelectionSort test = new SelectionSort();
+		arr = test.basicSelectionSort(arr);
+		assertArrayEquals("DT Failed", Sortedarr, arr);
+
 
 		}
 	}
